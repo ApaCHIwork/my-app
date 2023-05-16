3a9f19bd59099e5e942a54ab1b24351e99884ba4
ApaCHIwork <petrusvitaliywork@gmail.com>
2023-05-16 13:42:19 +0300

    add style to topbutton

--- src/components/TopButtons.jsx
+++ src/components/TopButtons.jsx
@@ -49,20 +49,35 @@ function TopButtons({ handleButtonClick }) {
   }
 
   return (
-    <div className=" flex items-center justify-between p-7 my-6">
+    <div className="flex items-center justify-between p-6 mt-4 rounded-xl bg-violet-400">
       {cities.map((city, index) => (
         <div key={index}>
           {city.info && (
             <button
               onClick={() => handleButtonClick(city.title)}
               key={city.id}
-              className="text-white text-lg font-medium m-4"
+              className="text-white text-sm font-medium"
             >
-              {city.title}
-              <small className="ml-2">
-                {Math.round(city.info.main.temp)}
-                <sup> o</sup>C
-              </small>
+              <div className="text-center">
+                  <span className="text-violet-200">
+                    {city.title}
+                  </span>
+                  <img
+                    src={`https://openweathermap.org/img/wn/${city.info.weather[0].icon}.png`}
+                    alt="test"
+                    className="mx-auto"
+                  />
+                  <p className="text-center">
+                    <span className="text-1xl mr-2" id="temperature">
+                      {Math.round(city.info.main.temp_max)}
+                      <sup>o</sup>
+                    </span>
+                    <span className="text-1xl text-violet-200" id="temperature">
+                      {Math.round(city.info.main.temp_min)}
+                      <sup>o</sup>
+                    </span>
+                  </p>
+                </div>
             </button>
           )}
         </div>
